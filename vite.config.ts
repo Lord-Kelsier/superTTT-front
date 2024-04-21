3feb18944a2de27c0ab0f505f0324d6b786b2085
Eduardo Contreras <econtreraslazcano@uc.cl>
2024-04-21 23:21:00 +0000

    build(vite configuration file): added environment variables for the serving port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: true,
+      port: parseInt(env.VITE_PORT),
+      watch: {
+        usePolling: true,
+        // for windows (wsl2) refresh on save
+      },
+    },
+  });
+};
